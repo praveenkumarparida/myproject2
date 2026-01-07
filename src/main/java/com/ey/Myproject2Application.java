0f678a48ed0dcc683c7bd6fe681fd9c1d68e8918
praveenkumarparida <praveenlaehss1@gmail.com>
2026-01-07 12:28:56 +0530

    first commit

---
+++ src/main/java/com/ey/Myproject2Application.java
@@ -0,0 +1,13 @@
+package com.ey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject2Application.class, args);
+	}
+
+}
